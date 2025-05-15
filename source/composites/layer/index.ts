09337ad6f28489beec32644700e44345c66c1c46
Protorians <129060085+protorians@users.noreply.github.com>
2025-05-15 11:06:12 +0000

    Add `outline` and `blurred` props to ThemeLayer
    Introduce `outline` for border styling and `blurred` to support gradient layers. Updated ThemeLayer logic to handle these props and adjust styles dynamically, enhancing flexibility and visual customization.

--- source/composites/layer/index.ts
+++ source/composites/layer/index.ts
@@ -9,6 +9,7 @@ import {
 import {ThemeLayerProps} from "./type.js";
 import {ITheme} from "../../types/index.js";
 import {LayerVariant} from "../../enums.js";
+import {createMultipleLayerGradient} from "../../utilities/index.js";
 
 export function ThemeLayer(
     theme: ITheme,
@@ -19,21 +20,50 @@ export function ThemeLayer(
         declaration,
         extended
     } = declarationExplodes<IWidgetDeclaration<HTMLElement, ThemeLayerProps & ICommonAttributes>, ThemeLayerProps>(
-        declarations, ['variant']
-    )
-    const coloring = theme.coloring(extended.variant || LayerVariant.Normal)
+        declarations, ['variant', 'outline', 'blurred'],
+    );
+    const variant = extended.variant || LayerVariant.Normal;
+    const coloring = theme[extended.outline ? 'outlineColoringResolves' : 'coloringResolves'](variant);
+    const isNude = !coloring.back;
 
     declaration.style = Style({
         ...theme.stylesheets.declarations
     })
         .merge(declaration.style)
         .merge({
-            color: Color[`${coloring.fore || 'tint'}`],
-            borderColor: Color[`${coloring.edge || 'tint-200-a1'}`],
-            backgroundColor: Color[`${coloring.back || 'tint-200-a4'}`],
+            position: "relative",
+            zIndex: '1',
         })
 
-    declaration.children = declaration.children || []
+    declaration.children = declaration.children || [];
 
-    return Layer(declaration)
+    return Layer({
+        style: {
+            position: "relative",
+            borderWidth: extended.outline ? 'var(--widget-border-width, 2px)' : '0',
+            color: Color[`${coloring.fore || 'tint'}`],
+            borderColor: Color[`${coloring.edge || 'tint-200-a1'}`],
+            backgroundColor: coloring.back ? Color[coloring.back] : 'transparent',
+            borderRadius: theme.settings.radiusMin || '0',
+            boxShadow: isNude ? 'none' : `${theme.stylesheets.declarations.boxShadow}`,
+        },
+        children: [
+            Layer({
+                style: {
+                    position: 'absolute',
+                    zIndex: '0',
+                    inset: '0',
+                    borderRadius: 'inherit',
+                    backgroundColor: 'transparent',
+                    overflow: 'hidden',
+                },
+                children: extended.blurred ?
+                    createMultipleLayerGradient(Array.isArray(extended.blurred) ? extended.blurred : [])
+                    : undefined,
+            }),
+            Layer({
+                ...declaration,
+            })
+        ]
+    });
 }
--- source/composites/layer/type.ts
+++ source/composites/layer/type.ts
@@ -1,6 +1,9 @@
 import {LayerVariant} from "../../enums.js";
+import {ILayerGradient} from "../../types/index.js";
 
 
 export type ThemeLayerProps = {
     variant?: LayerVariant;
+    outline?: boolean;
+    blurred?: boolean | ILayerGradient[];
 }
