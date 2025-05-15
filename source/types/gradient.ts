d2e13a5d0e3a7f6f098d28aade91ea49be440f92
Protorians <129060085+protorians@users.noreply.github.com>
2025-05-15 11:01:48 +0000

    Add ILayerGradient interface in gradient types
    Introduce a new interface, `ILayerGradient`, to define gradient properties such as color, radius, position, animation, opacity, and duration. This provides a structured way to manage and extend gradient-related configurations.

---
+++ source/types/gradient.ts
@@ -0,0 +1,9 @@
+
+export interface ILayerGradient {
+    color: string;
+    radius?: number;
+    position?: [number, number];
+    animated?: boolean;
+    opacity?: number;
+    duration?: number;
+}
