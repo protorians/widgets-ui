4fc9617804adf1bd0e4db7e89a0653b92a2d9f2d
Protorians <129060085+protorians@users.noreply.github.com>
2025-05-15 11:03:47 +0000

    Refactor alert component with enhanced capabilities
    Introduced new state management and capability methods for the alert component, including show, hide, expand, collapse, and destroy. Added support for collapsable layouts, customizable animations, and improved action handling. This update refines functionality while enhancing flexibility and extensibility.

--- source/composites/alert/index.ts
+++ source/composites/alert/index.ts
@@ -1,52 +1,166 @@
 import {
+    Column, createRef,
     declarationExplodes,
-    IChildren,
     ICommonAttributes,
-    IWidgetDeclaration,
+    IWidgetDeclaration, IWidgetNode, Row, Stack, Style,
 } from "@protorians/widgets";
-import {ThemeAlertProps} from "./type.js";
-import {ThemeDialog} from "../dialog/index.js";
+import {IThemeAlertCapability, IThemeAlertExtended, ThemeAlertProps} from "./type.js";
 import {ITheme} from "../../types/index.js";
-import {IModal} from "../../kits/index.js";
+import {createCapability} from "@protorians/core";
+import {AlertState} from "./enum.js";
 
 
 export function ThemeAlert(
     theme: ITheme,
     declarations: IWidgetDeclaration<HTMLElement, ICommonAttributes & ThemeAlertProps>
-): IChildren<any> {
+): IWidgetNode<any, any> | undefined {
 
-    const {extended} = declarationExplodes<IWidgetDeclaration<HTMLElement, ICommonAttributes & ThemeAlertProps>, ThemeAlertProps>(
+    const contentRef = createRef();
+
+    const {
+        declaration,
+        extended
+    } = declarationExplodes<IWidgetDeclaration<HTMLElement, ICommonAttributes & ThemeAlertProps>, ThemeAlertProps>(
         declarations, [
             'variant',
-            'scoped',
-            'trigger',
+            'direction',
+            'outline',
             'children',
-            'ariaTitle',
-            'ariaDescription',
-            'opened',
             'icon',
-            'actions',
-            'title',
+            'helmet',
             'animateIn',
-            'animateOut'
+            'animateOut',
+            'actions',
+            'collapsable',
+            'collapseSize',
+            'mounted',
+            'unmounted',
         ]
     );
+    extended.collapseSize = extended.collapsable ? extended.collapseSize || 80 : undefined;
 
-    return ThemeDialog(theme, {
-        ...extended,
-        trigger: extended.trigger,
-        children: extended.children,
-        actions: (dialog: IModal) => {
-            return [
-                theme.Button({
-                    variant: extended.variant,
-                    onPress: () => {
-                        dialog.close();
-                        extended.actions?.callable(dialog);
-                    },
-                    children: extended.actions?.trigger || ('Close'),
-                }),
-            ]
-        }
+    declaration.style = Style({})
+        .merge(declaration.style)
+        .merge({
+            display: 'flex',
+            flexDirection: extended.direction || 'row',
+            paddingX: 1,
+            paddingY: .8,
+            gap: 1,
+        })
+
+    const updateState = (state: AlertState) => {
+        instance.state = state;
+        contentRef.current?.attributeLess({'ui:state': `${state}`})
+    }
+
+    const {capability, current: instance} = createCapability<IThemeAlertCapability, IThemeAlertExtended>({
+        methods: ['show', 'destroy', 'toggle', 'hide', 'expand', 'collapse',]
+    });
+
+    instance.state = AlertState.Hidden;
+
+    capability.apply('show', () => {
+        widget.style({display: 'flex'})
+        if (extended.animateIn && widget)
+            theme.animate(widget, extended.animateIn).play()
+                .signal.listen('complete', () => {
+                updateState(AlertState.Shown);
+            })
+        return instance;
     })
+
+    capability.apply('hide', () => {
+        if (extended.animateOut && widget)
+            theme.animate(widget, extended.animateOut).play()
+                .signal.listen('complete', () => {
+                widget.style({display: 'none'});
+                updateState(AlertState.Hidden);
+            })
+        return instance;
+    })
+
+    capability.apply('destroy', () => {
+        if (extended.animateOut && widget)
+            theme.animate(widget, extended.animateOut).play()
+                .signal.listen('complete', () => {
+                widget.remove()
+                updateState(AlertState.Destroyed);
+            })
+        return instance;
+    })
+
+    capability.apply('expand', () => {
+        if (widget && extended.collapsable && contentRef.current) {
+            contentRef.current.style({
+                height: `${contentRef.current.clientElement!.scrollHeight}px`,
+            })
+            updateState(AlertState.Expand);
+        }
+        return instance;
+    })
+
+    capability.apply('collapse', () => {
+        if (widget && extended.collapsable && contentRef.current) {
+            contentRef.current.style({
+                height: `${extended.collapseSize}px`,
+            })
+            updateState(AlertState.Collapse);
+        }
+        return instance;
+    })
+
+    const widget = theme.Layer({
+        ...declaration,
+        variant: extended.variant,
+        outline: extended.outline,
+        children: [
+            Row({
+                children: [
+                    Row({
+                        style: Style({
+                            flex: '1 1 auto',
+                        }),
+                        children: extended.icon,
+                    }),
+                ]
+            }),
+            Column({
+                ref: contentRef,
+                style: {
+                    flex: '1 1 auto',
+                    transitionProperty: `all`,
+                    transitionTimingFunction: theme.settings.transitionTiming || 'ease-out',
+                    transitionDuration: theme.settings.transitionDuration || '360ms',
+                    overflowX: 'hidden',
+                    overflowY: extended.collapsable ? 'hidden' : 'initial',
+                    height: extended.collapsable ? '80px' : 'auto',
+                },
+                signal: {
+                    mount: () => updateState(instance.state)
+                },
+                children: [
+                    extended.helmet,
+                    extended.children,
+                ],
+            }),
+            Stack({
+                style: Style({
+                    gap: theme.settings.gap,
+                }),
+                children: extended.actions?.map(action => {
+                    return action.trigger
+                        .listen('click', () => action.callable(instance))
+                }),
+            })
+        ]
+    }) as IWidgetNode<any, any>;
+
+    widget.signal.listen('mount', () => {
+        instance.show();
+        (extended.mounted && extended.mounted(instance));
+    })
+    widget.signal.listen('unmount', () => (extended.unmounted && extended.unmounted(instance)))
+
+    return widget;
 }
--- source/composites/alert/type.ts
+++ source/composites/alert/type.ts
@@ -1,16 +1,38 @@
-import {IWidgetNode} from "@protorians/widgets";
-import {IModal, IModalOptions} from "../../kits/index.js";
+import {AligningDirection, IChildren, IWidgetNode} from "@protorians/widgets";
 import {LayerVariant} from "../../enums.js";
+import {IAnimetricSlimOptions} from "@protorians/animetric";
+import {IThemeAction} from "../../types/action.js";
+import {ICapabilityInstance} from "@protorians/core";
+import {AlertState} from "./enum.js";
 
 
-export interface IThemeAlertAction {
-    trigger: IWidgetNode<any, any>;
-    callable: (dialog: IModal) => void;
-}
-
-export interface ThemeAlertProps extends Partial<Omit<IModalOptions, 'favicon' | 'position' | 'locked' | 'blurred' | 'type'>> {
+export interface ThemeAlertProps {
     variant?: LayerVariant;
+    outline?: boolean;
+    closer?: boolean;
+    collapsable?: boolean;
+    collapseSize?: number;
+    direction?: AligningDirection;
     icon?: IWidgetNode<any, any>;
-    title?: string;
-    actions?: IThemeAlertAction;
+    helmet?: IWidgetNode<any, any>;
+    children?: IChildren<any>;
+    animateIn?: IAnimetricSlimOptions;
+    animateOut?: IAnimetricSlimOptions;
+    actions?: IThemeAction<IThemeAlert>[];
+    mounted?: (alerte: IThemeAlert) => void;
+    unmounted?: (alerte: IThemeAlert) => void;
 }
+
+export interface IThemeAlertCapability {
+    show: IThemeAlert;
+    hide: IThemeAlert;
+    destroy: IThemeAlert;
+    toggle: IThemeAlert;
+    expand: IThemeAlert;
+    collapse: IThemeAlert;
+}
+
+export type IThemeAlertExtended = {
+    state: AlertState;
+}
+export type IThemeAlert = ICapabilityInstance<IThemeAlertCapability> & IThemeAlertExtended
