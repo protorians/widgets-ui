6dd78c3be89cfb5ba1341c7a229c1be7afebe972
Protorians <129060085+protorians@users.noreply.github.com>
2025-05-25 19:19:28 +0000

    Add positioning utility and types for widget support
    Introduce `Positioning` class with an `alwaysOnScreen` method to manage widget positioning while keeping them in the viewport. Define corresponding types (`PositioningCoordinate` and `PositioningOptions`) to streamline the configuration and integration process.

---
+++ source/supports/positioning.ts
@@ -0,0 +1,48 @@
+import {IStyleSheetDeclarations, IWidgetNode} from "@protorians/widgets";
+import {PositioningCoordinate, PositioningOptions} from "../types/index.js";
+import {Callable, Environment} from "@protorians/core";
+
+
+export class Positioning {
+
+    static alwaysOnScreen(
+        widget: IWidgetNode<any, any>,
+        {gap, left, bottom, right, top, persist, origin}: PositioningOptions
+    ) {
+        const coordinate: PositioningCoordinate<string> = {};
+        gap = gap || 0;
+
+        if (left) coordinate.left = `${left}px`;
+        if (right) coordinate.right = `${right}px`;
+        if (top) coordinate.top = `${top}px`;
+        if (bottom) coordinate.bottom = `${bottom}px`;
+
+        if (Environment.Client) {
+            widget
+                .style(coordinate as IStyleSheetDeclarations)
+                .signal.listen('hide', () => widget.style({left: '', top: '', right: '', bottom: '',}))
+
+            function apply() {
+                const measure = widget.measure;
+                const originMeasure = origin?.measure;
+                const vw = Math.max(originMeasure?.width || 0, window.innerWidth);
+                const vh = Math.max(originMeasure?.height || 0, window.innerHeight);
+
+                if (measure.right > vw) widget.style({
+                    left: '',
+                    right: `${gap}px`,
+                })
+
+                if (measure.bottom > vh) widget.style({
+                    top: '',
+                    bottom: `${gap}px`,
+                })
+
+                if (persist) window.addEventListener('resize', apply)
+            }
+
+            Callable.safe(apply)
+        }
+    }
+
+}
---
+++ source/types/positioning.ts
@@ -0,0 +1,14 @@
+import {IWidgetNode} from "@protorians/widgets";
+
+export interface PositioningCoordinate<T> {
+    left?: T;
+    top?: T;
+    right?: T;
+    bottom?: T;
+}
+
+export interface PositioningOptions extends PositioningCoordinate<number> {
+    gap?: number;
+    persist?: boolean;
+    origin?: IWidgetNode<any, any>;
+}
